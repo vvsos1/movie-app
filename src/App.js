a0b718d43a587d0e1e155c57f15ac5d42eb345eb
vvsos1 <vvsos10@naver.com>
2021-01-02 19:58:17 +0900

    complete course

--- src/App.js
+++ src/App.js
@@ -1,55 +1,18 @@
-import axios from "axios";
-import { Component } from "react";
-import Movie from "./Movie";
+import { HashRouter, Route } from 'react-router-dom';
+import Navigation from './components/Navigation';
+import About from './routes/About';
+import Home from './routes/Home';
 import './App.css';
-class App extends Component {
-  state = {
-        isLoading: true,
-        movies: [],
-    };
+import Detail from './components/Detail';
 
-    getMovies = async () => {
-        const {
-            data: {
-                data: { movies },
-            },
-        } = await axios.get(
-            "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
-        );
-
-        this.setState({ movies, isLoading: false });
-    };
-
-    componentDidMount() {
-        this.getMovies();
-    }
-
-    render() {
-        const { movies, isLoading } = this.state;
-        return (
-            <section className="container">
-                {isLoading ? (
-                    <div className="loader">
-                        <span className="loader__text">Loading...</span>
-                    </div>
-                ) : (
-                    <div className="movies">
-                        {movies.map((movie) => (
-                            <Movie
-                                key={movie.id}
-                                id={movie.id}
-                                year={movie.year}
-                                title={movie.title}
-                                summary={movie.summary}
-                                poster={movie.medium_cover_image}
-                                genres={movie.genres}
-                                
-                            />
-                        ))}
-                    </div>
-                )}
-            </section>
-        );
-    }
+const App = () => {
+    return (
+    <HashRouter>
+        <Navigation />
+        <Route path="/" component={Home} exact={true} /> 
+        <Route path="/about" component={About}/>
+        <Route path="/movie-detail" component={Detail}/>
+    </HashRouter>)
 }
+
 export default App;
---
+++ src/components/Navigation.js
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+import "./Navigation.css";
+
+
+const Navigation = () => {
+    return (
+    <div className="nav">
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+    </div>
+    )
+}
+
+export default Navigation;
